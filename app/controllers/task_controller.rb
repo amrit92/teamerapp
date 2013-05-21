43b5035a441d90584352a256c3419badf4cb39e6
vvvardhan <vishnu.vinjam@gmail.com>
2013-05-21 15:38:15 +0530

    Mock methods for task_controllers

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,11 +1,20 @@
 class TaskController < ApplicationController
 
+#before_filter signed_in?   && belongs_to_event??
+
 	def create
-		
+		@task = current_event.task.build(params[:task])
+		@task.user_id = current_user.id
+		if @task.save
+			flash[:success] = "New Task Created"
+		else
+			render 'pages/home'
+		end
 	end
 
-	def destroy
-		
+	def destroy   #should check if the current_user is same as creator user_id
+		@task = Task.find(params[:id])
+    	@task.destroy
 	end
 
 	def show
