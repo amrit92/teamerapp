19145b18de0c4ae216ffb2ca9faa9f1f728550df
vvvardhan <vishnu.vinjam@gmail.com>
2013-05-21 18:58:03 +0530

    ruby 1.9.3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,15 @@
 require 'digest'
+
 class User < ActiveRecord::Base
 attr_accessor :password
 attr_accessible :name, :email, :password, :password_confirmation
 has_many :events, :dependent => :destroy
 has_many :tasks, :dependent => :destroy
 has_many :follows, :foreign_key => "follower_id", :dependent => :destroy
-has_many :takens, :foreign_key => “taker_id”,
+has_many :takens, :foreign_key => "taker_id",
 				  :dependent => :destroy
 
-has_many  :doing_tasks, :through => takes, 
+has_many  :doing_tasks, :through => :takens, 
 						:source => :taken
 
 email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
